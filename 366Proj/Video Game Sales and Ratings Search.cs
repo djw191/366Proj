953aff817b6b5c630f5798fc67f1dc49a4dfae03
brycerr <94137678+brycerr@users.noreply.github.com>
2021-12-10 21:13:49 -0600

    Basic View Review Panels
    The database isn't being recognized for some reason, so I don't know if the SQLite stuff is working as intended.
    
    I have spent a lot of time trying to figure out what the problem is.

--- 366Proj/Video Game Sales and Ratings Search.cs
+++ 366Proj/Video Game Sales and Ratings Search.cs
@@ -21,6 +21,11 @@ namespace _366Proj
         string minMetascoreString = "";
         string ratingString = "";
         string platformString = "";
+
+        // Bryce Variables
+        int yourScore = 0;      // the score of the review being made/edited\
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -212,6 +217,120 @@ namespace _366Proj
                 }
                 conn.Close();
             }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createUserReviews_Save_Click(object sender, EventArgs e)  // Create Your Review Button
+        {
+            insertDataIntoTable("YourReviews", "(Name, Platform, yourReview, yourScore)", "(TEST_NAME, TEST_PLATFORM, TEST_TEXT, 7)");
+            Console.WriteLine("success");
+        }
+
+        private void label4_Click_1(object sender, EventArgs e) // Create Review Game title (displayed)
+        {
+
+        }
+
+        private void createUserReviews_Back(object sender, EventArgs e)     // Back button for createUserReviews panel
+        {
+            viewUserReviewsPanel.Visible = true;
+            createUserReviewsPanel.Visible = false;
+        }
+
+        private void view_Click_1(object sender, EventArgs e)   // main panel -> "view your reviews" button
+        {
+            viewUserReviewsPanel.Visible = true;
+            mainPagePanel.Visible = false;
+            resultsPage.Visible = false;
+        }
+
+        private void viewUserReviews_Back_Click(object sender, EventArgs e)
+        {
+            mainPagePanel.Visible = true;
+            resultsPage.Visible = false;
+            viewUserReviewsPanel.Visible = false;
+        }
+
+        private void viewUserReviews_EDIT_REVIEW_Click(object sender, EventArgs e)
+        {
+            viewUserReviewsPanel.Visible = false;
+            createUserReviewsPanel.Visible = true;
+
+            // TODO: load createUserReviewsPanel with info from selected review
+        }
+
+        private void createUserReviewsPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void createUserReview_Delete_Click(object sender, EventArgs e)      // delete button
+        {
+
+        }
+
+        private void createUserReview_score_dropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            yourScore = 10 - createUserReview_score_dropdown.SelectedIndex;     // gets the number that the user selected from the dropdown list
+        }
+
+        public void PopulateAnyTable(string query, DataGridView dataGrid)       // should be exactly the same as PopulateTable(), but this one accepts a dataGridView as a parameter
+        {
+            Console.WriteLine(query);
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+                var da = new SQLiteDataAdapter(query, conn);
+                DataTable dtbl = new DataTable();
+                da.Fill(dtbl);
+                dataGrid.DataSource = dtbl;
+                dataGrid.Update();
+                dataGrid.Refresh();
+                conn.Close();
+            }
+        }
+
+        private void insertDataIntoTable(string table, string column, string inputData)  // Inserts data into specified table
+            // Fornat for column:    "(column1, column2, etc.)"
+            // Format for inputData: "(value1, value2, etc.);"
+        {
+            Console.WriteLine(table);
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                try {
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.CommandText = @"INSERT INTO " + table + " " + column + " VALUES " + inputData;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+
+                    int i = cmd.ExecuteNonQuery();
+
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Table: " + table + "\nInput Data: " + inputData);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+
+        private void getYourReviews()
+        {
+            PopulateAnyTable("SELECT name, Platform, yourScore" +
+                "FROM YourReviews", viewUserReviews_dataGridView);
+        }
+
+        private void viewUserReviews_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }

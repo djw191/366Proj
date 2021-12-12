198be551acdce86fd2fea6b2c9c952a64c48fcb2
brycerr <94137678+brycerr@users.noreply.github.com>
2021-12-12 00:46:24 -0600

    Revert "Revert "More UI + SQLite""
    This reverts commit a33eaecb89e16a005f81466128ce542ea85c919a.

--- 366Proj/Video Game Sales and Ratings Search.cs
+++ 366Proj/Video Game Sales and Ratings Search.cs
@@ -24,10 +24,8 @@ namespace _366Proj
 
         // Bryce Variables
 
-        string yourReviewName = null;
-        string yourReviewPlatform = null;  
-        string yourReviewText = null;
         int yourScore = 0;      // the score of the review being made/edited
+        String[] currentReview = {"", "", ""}; // [0] = name of game, [1] = platform of game, [2] = review text;
 
         // End of Bryce Variables
 
@@ -233,7 +231,7 @@ namespace _366Proj
 
         public void PopulateAnyTable(string query, DataGridView dataGrid)       // should be exactly the same as PopulateTable(), but this one accepts a dataGridView parameter for use with any dataGridView
         {
-            Console.WriteLine("PopulateAnyTable: " + query);
+            Console.WriteLine(query);
             using (SQLiteConnection conn = new SQLiteConnection(connString))
             {
                 try
@@ -257,12 +255,38 @@ namespace _366Proj
             }
         }
 
+        private void setReviewText(string Name, string Platform)    // used for filling textbox when user opens an existing review
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                try
+                {
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.CommandText = @"SELECT Name, Platform, yourReview, yourScore FROM YourReviews WHERE Name = '" + Name + "' AND Platform = '" + Platform + "'";
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    SQLiteDataReader r = cmd.ExecuteReader();
+                    r.Read();
+                    currentReview[2] = r.GetValue(2).ToString();    // review text
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
         private void insertDataIntoTable(string table, string column, string inputData)  // Inserts data into specified table
                                                                                          // Format for column:    "(column1, column2, etc.)"
                                                                                          // Format for inputData: "(value1, value2, etc.);"
-                                                                                         // Example code: insertDataIntoTable("YourReviews", "(Name, Platform, YourScore)", "(Wii Sports, WII, 9)");
-                                                                                            // Creates a review for Wii Sports on the Wii with a score of 9
-        {
+                                                                                         // Example code: insertDataIntoTable("YourReviews", "(Name, Platform, YourScore)", "('Wii Sports', 'WII', 9)");
+                                                                                         // Creates a review for Wii Sports on the Wii with a score of 9
+        { 
             Console.WriteLine(table);
             using (SQLiteConnection conn = new SQLiteConnection(connString))
             {
@@ -292,52 +316,47 @@ namespace _366Proj
             }
         }
 
+        private void deleteDataFromTable(String table, String statement)
+        {
+            Console.WriteLine(table);
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                try
+                {
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.CommandText = @"DELETE FROM " + table + " WHERE " + statement;
+                    cmd.Connection = conn;
+
+                    conn.Open();
+
+                    int i = cmd.ExecuteNonQuery();
+
+                    if (i == 1)
+                    {
+                        MessageBox.Show("DELETED FROM " + table + " WHERE " + statement);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
         private void getYourReviews()
         {
             PopulateAnyTable("SELECT Name, Platform, yourScore " +
                 "FROM YourReviews", viewUserReviews_dataGridView);
         }
 
-        private void setReviewText(string Name, string Platform)    // used for filling textbox when user opens an existing review
-        {
-            using (SQLiteConnection conn = new SQLiteConnection(connString))
-            {
-                using (var cmd = new SQLiteCommand())
-                {
-                    try
-                    {
-                        cmd.CommandText = @"SELECT Name, Platform, yourReview, yourScore FROM YourReviews WHERE Name = '" + Name + "' AND Platform = '" + Platform + "'";
-                        cmd.Connection = conn;
-                        conn.Open();
-
-                        SQLiteDataReader r = cmd.ExecuteReader();
-                        r.Read();
-                        yourReviewText = r.GetValue(2).ToString();    // review text
-                    }
-                    catch (Exception e)
-                    {
-                        MessageBox.Show(e.Message);
-                    }
-                    finally
-                    {
-                        cmd.Dispose();
-                        conn.Close();
-                    }
-                }
-            }
-        }
-
-        private void createUserReviews_Back(object sender, EventArgs e) // Back button for createUserReviews panel
-        {
-            viewUserReviewsPanel.Visible = true;
-            createUserReviewsPanel.Visible = false;
-        }
-
-        private void view_Click_1(object sender, EventArgs e)   // main panel -> "view your reviews" button
+        private void view_Click_1(object sender, EventArgs e)                   // main panel -> "view your reviews" button
         {
             viewUserReviewsPanel.Visible = true;
             mainPagePanel.Visible = false;
-            resultsPage.Visible = false;
 
             getYourReviews();
         }
@@ -345,39 +364,59 @@ namespace _366Proj
         private void viewUserReviews_Back_Click(object sender, EventArgs e)
         {
             mainPagePanel.Visible = true;
-            resultsPage.Visible = false;
             viewUserReviewsPanel.Visible = false;
-        }
-
-        private void viewUserReviews_NEW_REVIEW_Click(object sender, EventArgs e)
-        {
-            viewUserReviewsPanel.Visible = false;
-            createUserReviewsPanel.Visible = true;
         }
 
         private void viewUserReviews_EDIT_REVIEW_Click(object sender, EventArgs e)
         {
-            viewUserReviewsPanel.Visible = false;
-            createUserReviewsPanel.Visible = true;
+            if (currentReview[0] != "" && currentReview[1] != "")
+            {
+                viewUserReviewsPanel.Visible = false;
+                createUserReviewsPanel.Visible = true;
 
-            // TODO: call createUserReviewsPanel with info from the selected review
-            createReviewGameTitle.Text = yourReviewName;
+                // Call createUserReviewsPanel with the info from the selected review
+                createReviewGameTitle.Text = currentReview[0];
+                createReviewPlatform.Text = currentReview[1];
+                createUserReview_TextReview.Text = currentReview[2];
+                createReviewGameTitle.Refresh();
+
+
+            }
         }
 
         private void createUserReviewsPanel_Paint(object sender, PaintEventArgs e)
         {
-
+            
         }
 
-        private void createUserReviews_Save_Click(object sender, EventArgs e)  // Create Your Review Button
+        private void createUserReviews_Back(object sender, EventArgs e)         // Back button for createUserReviews panel
         {
-            insertDataIntoTable("YourReviews", "(Name, Platform, yourReview, yourScore)", "('TEST_NAME', 'TEST_PLATFORM', 'TEST_TEXT', " + yourScore + ")");
-            Console.WriteLine("success");
+            viewUserReviewsPanel.Visible = true;
+            createUserReviewsPanel.Visible = false;
         }
 
-        private void createUserReview_Delete_Click(object sender, EventArgs e)      // delete button
+        private void createUserReviews_Save_Click(object sender, EventArgs e)   // Save button
         {
-            // delete the user's selected review from database
+            // TODO: NEED TO CHECK IF THE REVIEW FOR A GAME ALREADY EXISTS
+
+            if (currentReview[0] != "" && currentReview[1] != "")
+            {
+                String textReview = createUserReview_TextReview.Text;
+                insertDataIntoTable("YourReviews", "(Name, Platform)", "('bruh', 'moment')");
+
+                //insertDataIntoTable("YourReviews", "(Name, Platform, yourReview, yourScore)", "('" + currentReview[0] + "', '" + currentReview[1] + "', '" + textReview + "', " + yourScore + ")");
+                Console.WriteLine("success");
+            }
+        }
+
+        private void createUserReview_Delete_Click(object sender, EventArgs e)  // Delete button
+        {
+            // Could improve this by making a function specificially for YourReviews
+            if (currentReview[0] != "" && currentReview[1] != "")
+            {
+                deleteDataFromTable("YourReviews", "Name = " + currentReview[0] + " AND Platform = " + currentReview[1]);
+            }
+            //createUserReviews_Back();
         }
 
         private void createUserReview_score_dropdown_SelectedIndexChanged(object sender, EventArgs e)
@@ -387,11 +426,23 @@ namespace _366Proj
 
         private void viewUserReviews_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            // when user clicks on cell, record the cell content
-            yourReviewName = viewUserReviews_dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
-            yourReviewPlatform = viewUserReviews_dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            currentReview[0] = viewUserReviews_dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString(); // name
+            currentReview[1] = viewUserReviews_dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString(); // platform
+            currentReview[2] = viewUserReviews_dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString(); // review text
 
-            setReviewText(yourReviewName, yourReviewPlatform);
+            setReviewText(currentReview[0], currentReview[1]);
+        }
+
+        private void add_Click(object sender, EventArgs e)  // main panel -> add new game button
+        {
+            addNewGamePanel.Visible = true;
+            mainPagePanel.Visible = false;
+        }
+
+        private void addNewGame_Back_Click(object sender, EventArgs e)
+        {
+            mainPagePanel.Visible = true;
+            addNewGamePanel.Visible = false;
         }
     }
 }
